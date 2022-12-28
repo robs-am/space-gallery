a1f5f729253a9974f1262f057df19eaab4b08bb4
robs-am <roberta.amaro89@gmail.com>
2022-12-28 18:12:14 -0300

    add icones menu

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,27 +1,32 @@
 import React from "react";
+import inicio from "../../assets/icones/inicio.svg"
+import curtidas from "../../assets/icones/curtidas.svg"
+import novas from "../../assets/icones/novas.svg"
+import vistas from "../../assets/icones/vistas.svg"
+import surpreenda from "../../assets/icones/surpreenda.svg"
 
 export default function Menu() {
   return (
     <nav>
       <ul>
         <li>
-          <img src="" alt="" />
+          <img src={inicio} alt="" />
           <a href="/">Home</a>
         </li>
         <li>
-          <img src="" alt="" />
+          <img src={curtidas} alt="" />
           <a href="/">Mais curtidas</a>
         </li>
         <li>
-          <img src="" alt="" />
+          <img src={vistas} alt="" />
           <a href="/">Mais Vistas</a>
         </li>
         <li>
-          <img src="" alt="" />
+          <img src={novas} alt="" />
           <a href="/">Novas</a>
         </li>
         <li>
-          <img src="" alt="" />
+          <img src={surpreenda} alt="" />
           <a href="/">Surpreenda-me</a>
         </li>
       </ul>
