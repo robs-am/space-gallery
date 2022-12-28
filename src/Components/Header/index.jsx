86960ff53e68608e057f3cea281b190d0b6131eb
robs-am <roberta.amaro89@gmail.com>
2022-12-28 13:53:02 -0300

    add components header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,5 +1,16 @@
+import logo from './logo2.svg';
+import search from './search.png'
+
+
 export default function Header() {
     return (
-        <h1>Header</h1>
+        <header>
+            <img src={logo} size={12} alt="Logo da Galeria Espacial" />
+            <div>
+                <input type="text"
+                placeholder="O que você procura?"/>
+                <img src={search} alt="ícone de lupa" />
+            </div>
+        </header>
     )
 }
