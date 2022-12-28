a03d23d8736379468096b2f9c0ec482951cf37f3
robs-am <roberta.amaro89@gmail.com>
2022-12-28 19:05:51 -0300

    add banner

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -5,27 +5,29 @@ import novas from "../../assets/icones/novas.svg"
 import vistas from "../../assets/icones/vistas.svg"
 import surpreenda from "../../assets/icones/surpreenda.svg"
 
+import styles from "./Menu.module.scss"
+
 export default function Menu() {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className={styles.menu}>
+      <ul className={styles.menu__lista}>
+        <li className={styles.menu__item}>
           <img src={inicio} alt="" />
           <a href="/">Home</a>
         </li>
-        <li>
+        <li className={styles.menu__item}>
           <img src={curtidas} alt="" />
           <a href="/">Mais curtidas</a>
         </li>
-        <li>
+        <li className={styles.menu__item}>
           <img src={vistas} alt="" />
           <a href="/">Mais Vistas</a>
         </li>
-        <li>
+        <li className={styles.menu__item}>
           <img src={novas} alt="" />
           <a href="/">Novas</a>
         </li>
-        <li>
+        <li className={styles.menu__item}>
           <img src={surpreenda} alt="" />
           <a href="/">Surpreenda-me</a>
         </li>
--- src/Pages/Homepage/index.jsx
+++ src/Pages/Homepage/index.jsx
@@ -1,11 +1,23 @@
 import Menu from "../../Components/Menu";
 import Header from "./../../Components/Header/index";
 
+import Banner from "./Banner.png"
+
+import styles from "./Homepage.module.scss";
+
 export default function Homepage() {
   return (
     <>
       <Header />
-      <Menu />
+      <main>
+        <section>
+          <Menu />
+          <div>
+            <h1>galeria</h1>
+            <img src={Banner} alt="Imagem de cristais em um planeta AI" />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
