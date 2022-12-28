687ec2e60ee8690252e5b31b37e5e8af0cfba7e5
robs-am <roberta.amaro89@gmail.com>
2022-12-28 15:21:49 -0300

    add menu component

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export default function Menu() {
   return (
-    <div>Menu</div>
-  )
+    <nav>
+      <ul>
+        <li>
+          <img src="" alt="" />
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <a href="/">Mais curtidas</a>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <a href="/">Mais Vistas</a>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <a href="/">Novas</a>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <a href="/">Surpreenda-me</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
