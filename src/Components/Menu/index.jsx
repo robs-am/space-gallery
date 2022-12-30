6d117db4e83973ea03dd662ff5cf62d73a7aa31f
robs-am <roberta.amaro89@gmail.com>
2022-12-30 08:25:23 -0300

    add novos icones

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import inicio from "../../assets/icones/inicio.svg"
-import curtidas from "../../assets/icones/curtidas.svg"
-import novas from "../../assets/icones/novas.svg"
-import vistas from "../../assets/icones/vistas.svg"
-import surpreenda from "../../assets/icones/surpreenda.svg"
+import inicio2 from "../../assets/icones/inicio2.svg"
+import curtidas2 from "../../assets/icones/curtidas2.svg"
+import novas2 from "../../assets/icones/novas2.svg"
+import vistas2 from "../../assets/icones/vistas2.svg"
+import surpreenda2 from "../../assets/icones/surpreenda2.svg"
 
 
 import styles from "./Menu.module.scss"
@@ -14,23 +14,23 @@ export default function Menu() {
     <nav className={styles.menu}>
       <ul className={styles.menu__lista}>
         <li className={styles.menu__item}>
-          <img src={inicio}  alt="" />
+          <img src={inicio2}  alt="" />
           <a href="/">Home</a>
         </li>
         <li className={styles.menu__item}>
-          <img src={curtidas} alt="" />
+          <img src={curtidas2} alt="" />
           <a href="/">Mais curtidas</a>
         </li>
         <li className={styles.menu__item}>
-          <img src={vistas} alt="" />
+          <img src={vistas2} alt="" />
           <a href="/">Mais Vistas</a>
         </li>
         <li className={styles.menu__item}>
-          <img src={novas} alt="" />
+          <img src={novas2} alt="" />
           <a href="/">Novas</a>
         </li>
         <li className={styles.menu__item}>
-          <img src={surpreenda} alt="" />
+          <img src={surpreenda2} alt="" />
           <a href="/">Surpreenda-me</a>
         </li>
       </ul>
