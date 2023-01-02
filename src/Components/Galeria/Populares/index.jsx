d43d5f40705333aaf654ece40ff99e79a219cf98
robs-am <roberta.amaro89@gmail.com>
2023-01-02 19:49:20 -0300

    add função filtragem de fotos

--- src/Components/Galeria/Populares/index.jsx
+++ src/Components/Galeria/Populares/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import fotosPoupulares from "./fotos-populares.json";
+//import fotosPopulares from "./fotos-populares.json";
 
 export default function Populares() {
   return (
     <asided>
       <h2>Populares</h2>
+      <p>teste</p>
       <ul></ul>
     </asided>
   );
