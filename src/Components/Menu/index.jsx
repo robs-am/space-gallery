03b35351a1c98ae4ff9d2b7c166e2d4c88d3a72b
Roberta Amaro <roberta.amaro89@gmail.com>
2023-01-07 06:20:19 -0300

    delete icone surpreendame

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,21 +1,17 @@
 import React from "react";
-import inicio2 from "../../assets/icones/inicio2.svg"
-import curtidas2 from "../../assets/icones/curtidas2.svg"
-import novas2 from "../../assets/icones/novas2.svg"
-import vistas2 from "../../assets/icones/vistas2.svg"
-import surpreenda2 from "../../assets/icones/surpreenda2.svg"
+import inicio2 from "../../assets/icones/inicio2.svg";
+import curtidas2 from "../../assets/icones/curtidas2.svg";
+import novas2 from "../../assets/icones/novas2.svg";
+import vistas2 from "../../assets/icones/vistas2.svg";
 
-
-
-import styles from "./Menu.module.scss"
-
+import styles from "./Menu.module.scss";
 
 export default function Menu() {
   return (
     <nav className={styles.menu}>
       <ul className={styles.menu__lista}>
         <li className={styles.menu__item}>
-          <img src={inicio2}  alt="" />
+          <img src={inicio2} alt="" />
           <a href="/">Home</a>
         </li>
         <li className={styles.menu__item}>
@@ -29,10 +25,6 @@ export default function Menu() {
         <li className={styles.menu__item}>
           <img src={novas2} alt="" />
           <a href="/">Novas</a>
-        </li>
-        <li className={styles.menu__item}>
-          <img src={surpreenda2} alt="" />
-          <a href="/">Surpreenda-me</a>
         </li>
       </ul>
     </nav>
